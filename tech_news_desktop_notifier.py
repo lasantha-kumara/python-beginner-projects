b8b0655e1e59cd0741e79385add8c21c15c66bdd
Lasantha Mudalige <lasantha1130@gmail.com>
2022-07-31 20:53:14 +0530

    complete tech news notification app

--- tech_news_desktop_notifier.py
+++ tech_news_desktop_notifier.py
@@ -1,7 +1,7 @@
 import requests
 import notify2
 import time
-
+import datetime
 
 API_KEY = "0fb5d9190b5e409ab1e6ad41b9bffabc"
 
@@ -12,20 +12,33 @@ def main():
     data = response.json()
     articles = data["articles"]
 
-    notify2.init("News notification app")
-
-    titles = []
+    notify2.init("Tech news notification app")
 
     item_num = 1
     for article in articles:
-        title = f"{item_num} {article['title']}\n"
-        notification = notify2.Notification("Tech news today", title)
-        notification.show()
-        titles.append(title)
-        item_num += 1
-        time.sleep(5)
+        title = article['title']
+        description = article["description"]
 
-    print(titles)
+        if title == None:
+            title = "Empty title"
+        elif description == None:
+            description = "Empty description"
+
+        # show every titles in articles for every 10 seconds
+        notification = notify2.Notification(
+            f"{item_num}. {title}", description)
+        notification.show()
+        time.sleep(10)
+
+        # after showing news notification add it to a txt file
+        news = f"{item_num}. {title} {description}"
+        today = datetime.date.today()
+
+        with open(f"Tech news {today}.txt", "a") as file:
+            file.write(news)
+            file.write("\n\n")
+
+        item_num += 1
 
 
 main()
