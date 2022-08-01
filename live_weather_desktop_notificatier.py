e95c66d28b4523d34610fd4d72e4617bc71e2f69
Lasantha Mudalige <lasantha1130@gmail.com>
2022-08-01 17:40:12 +0530

    changed name of the file

--- live_weather_desktop_notificatier.py
+++ live_weather_desktop_notificatier.py
@@ -5,7 +5,7 @@ API_KEY = "b79d7a8fdab2aa98e5ff768a7872dc30"
 
 
 def main():
-    # get current geo location
+    # get current geo location using open weather geo location api
     city_name = "yattogoda".title()
     response = requests.get(
         f"https://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=5&appid={API_KEY}")
@@ -14,14 +14,19 @@ def main():
     latitude = location_data[0]["lat"]
     longitude = location_data[0]["lon"]
 
-    # get weather data of current location
+    # get weather data of current location using open weather api
     response = requests.get(
-        f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}")
+        f"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={API_KEY}&units=metric")
     weather_data = response.json()
     print(weather_data)
 
     # show current weather notification
     notify2.init("Weather notification app")
+
+    # weather_condition = weather_data["weather"]["main"]
+    # temp = weather_data["main"]["temp"]
+    # message = f"Current temperature in {city_name} is {temp} and it"
+
     notification = notify2.Notification("Weather update", )
     notification.show()
 
